7cc4a6aa05a6af6b12c2ac5d9bc938135cb0275f
thysmesi <77686907+thysmesi@users.noreply.github.com>
2024-03-11 18:47:09 -0600

    added app

---
+++ app/src/lib/constants.js
@@ -0,0 +1,18 @@
+export const NAVIGATION_MENU = {
+    "User Login" : {route: "/login"},
+    "User Sign Up" : {route: "/signup"},
+    "I am a Buddy" : {route: "/need"},
+    "I want to be a Buddy" : {route: "/volunteer"},
+}
+
+export const IMAGES = {
+    "home-quality-1": "pexels-rdne-stock-project-7403043.jpg",
+    "home-quality-2": "nathan-anderson-FHiJWoBodrs-unsplash (1).jpg",
+    "home-quality-3": "pexels-tara-winstead-8386122.jpg"
+}
+
+
+// export const palette = {
+    // background: "white",
+    // accent: "#4e888f"
+// }
